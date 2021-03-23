6bd1c2ee59c8c5ef1e00a64f3098f18a49f60e6d
KARAN-2007 <76608112+KARAN-2007@users.noreply.github.com>
2021-03-23 15:58:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj,groundObject,paper;
+var world;
+
+
+function setup() {
+	createCanvas(1600, 700);
+	
+
+
+	engine = Engine.create();
+	world = engine.world;
+	paper=new Paper(500,300,100,100);
+	groundObject=new ground(width/2,670,width,20);
+	dustbinObj=new dustbin(1200,650);
+	
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+ 
+  background("white");
+  groundObject.display(); 
+  paper.display();
+  dustbinObj.display();
+
+  
+  
+  
+
+}
+function keyPressed(){
+    if(keyCode === 32){
+		Matter.Body.applyForce(paper.body,0,{x:600,y:-500})
+		
+		
+	}
+}
